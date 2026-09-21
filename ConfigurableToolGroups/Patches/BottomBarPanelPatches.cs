4d519dc31bbee1504c45377c414d415b7cae7a8e
tharbad08 <10381553+tharbad08@users.noreply.github.com>
2026-09-21 09:47:39 +0300

    Fix toolbar overlap with Minimap

--- ConfigurableToolGroups/Patches/BottomBarPanelPatches.cs
+++ ConfigurableToolGroups/Patches/BottomBarPanelPatches.cs
@@ -3,6 +3,35 @@
 [HarmonyPatch(typeof(BottomBarPanel))]
 public static class BottomBarPanelPatches
 {
+    private const float MinimapCompatibilityOffset = -120f;
+    private const string MinimapAssemblyNameFragment = "Minimap";
+
+    [HarmonyPostfix, HarmonyPatch(nameof(BottomBarPanel.Load))]
+    public static void ApplyMinimapCompatibilityOffset(BottomBarPanel __instance)
+    {
+        if (!IsMinimapLoaded())
+        {
+            return;
+        }
+
+        __instance._mainElements.style.translate =
+            new UnityEngine.UIElements.Translate(MinimapCompatibilityOffset, 0f);
+    }
+
+    private static bool IsMinimapLoaded()
+    {
+        foreach (var assembly in System.AppDomain.CurrentDomain.GetAssemblies())
+        {
+            var assemblyName = assembly.GetName().Name;
+            if (assemblyName is not null &&
+                assemblyName.IndexOf(MinimapAssemblyNameFragment, System.StringComparison.OrdinalIgnoreCase) >= 0)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
 
     [HarmonyPrefix, HarmonyPatch(nameof(BottomBarPanel.InitializeLeftSection))]
     public static bool RearrangeLeftSection(BottomBarPanel __instance) => PatchSection(__instance, RootElementLocation.Left);
